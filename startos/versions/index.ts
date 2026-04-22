12d85ee90c348d238e0f448d2ea5371c0335c953
Shadowy Super Coder <musashidisciple@proton.me>
2026-04-22 11:09:08 -0600

    remove pre-release tag

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_10_0_1_b0 } from './v0.10.0.1.b0'
+import { v_0_10_0_1 } from './v0.10.0.1'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_10_0_1_b0,
+  current: v_0_10_0_1,
   other: [],
 })
--- startos/versions/v0.10.0.1.ts
+++ startos/versions/v0.10.0.1.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
-export const v_0_10_0_1_b0 = VersionInfo.of({
-  version: '0.10.0:1-beta.0',
+export const v_0_10_0_1 = VersionInfo.of({
+  version: '0.10.0:1',
   releaseNotes: {
     en_US: 'Initial StartOS release of Fedimint Gateway v0.10.0',
   },
