a9c7908d1aa60cdb28487ba85ffc1e9f52d86ab3
Helix <267227783+helix-nine@users.noreply.github.com>
2026-07-24 03:49:08 +0000

    chore: bump start-sdk to 2.0.7; refresh bitcoin-core-startos,lnd-startos pin(s); 0.11.1:3 → 0.11.1:4

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '0.11.1:3',
+  version: '0.11.1:4',
   releaseNotes: {
     en_US: 'Internal updates',
     es_ES: 'Actualizaciones internas',
